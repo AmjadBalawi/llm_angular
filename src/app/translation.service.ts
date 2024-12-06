4002721338db68c2c579c86b7d5ad6ac995f041f
AmjadBalawi <44215170+AmjadBalawi@users.noreply.github.com>
2024-12-06 21:54:40 +0100

    Update translation.service.ts

--- src/app/translation.service.ts
+++ src/app/translation.service.ts
@@ -11,7 +11,7 @@ interface TranslationRequest {
   providedIn: 'root'
 })
 export class TranslationService {
-  private apiUrl = 'https://mml-fastapi-qrndzw.fly.dev/translate'; // Update this URL if needed
+  private apiUrl = 'https://llm-fastapi.fly.dev/translate'; // Update this URL if needed
 
   constructor(private http: HttpClient) {}
 
