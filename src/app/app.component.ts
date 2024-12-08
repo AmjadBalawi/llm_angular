3ba55e7aacfff9a4fcf6d9c3f4d4aafc673adde9
AmjadBalawi <44215170+AmjadBalawi@users.noreply.github.com>
2024-12-08 01:39:06 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router'; // Importing RouterModule
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterModule]
+  imports: [RouterModule,CommonModule, FormsModule]
 })
 export class AppComponent {
   title = 'GoExplore';
+  isDonationSectionVisible: boolean = true;
+
+  // Method to hide the donation section
+  closeDonationSection() {
+    this.isDonationSectionVisible = false;
+    console.log('Donation section closed'); // Debugging log
+  }
 }
