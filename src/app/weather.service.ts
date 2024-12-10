b63871e8c9309b49bef469b4e452e7758202397d
AmjadBalawi <44215170+AmjadBalawi@users.noreply.github.com>
2024-12-10 01:31:12 +0100

    Update weather.service.ts

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class WeatherService {
-  private apiUrl = 'http://localhost:8000/weather'; // FastAPI endpoint
+  private apiUrl = 'https://fastapi-goexplore.vercel.app/weather'; // FastAPI endpoint
 
   constructor(private http: HttpClient) {}
 
