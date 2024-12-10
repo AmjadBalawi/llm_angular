18dd2a366458de30157b56f256dc468d49636c91
AmjadBalawi <44215170+AmjadBalawi@users.noreply.github.com>
2024-12-10 01:31:42 +0100

    Update destination-activities.service.ts

--- src/app/destination-activities.service.ts
+++ src/app/destination-activities.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class DestinationActivitiesService {
-  private apiUrl = 'http://127.0.0.1:8000/destination-activities';
+  private apiUrl = 'https://fastapi-goexplore.vercel.app/destination-activities';
 
   constructor(private http: HttpClient) {}
 
